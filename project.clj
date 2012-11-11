153fab2dfe138a139758e9dd2cba95158e7703a2
Bor Hodošček <bor.hodoscek@gmail.com>
2012-11-11 13:44:55 +0900

    updated dependencies; added server dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject natsume-clj "0.2.0-SNAPSHOT"
+(defproject natsume-clj "0.3.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "Natsume writing assistance system data processor and server"
   :url "http://hinoki.ryu.titech.ac.jp/natsume/"
@@ -11,9 +11,12 @@
                  [postgresql "9.1-901.jdbc4"]
                  [korma "0.3.0-beta11"]
                  [com.ibm.icu/icu4j "49.1"]
-                 [aleph "0.3.0-beta4"]
+                 [aleph "0.3.0-beta5"]
                  [org.apache.commons/commons-compress "1.4.1"]
-                 [com.taoensso/timbre "0.8.1"]
+                 [com.taoensso/timbre "1.0.0"]
                  [org.clojure/tools.cli "0.2.2"]
-                 [midje "1.5.0-SNAPSHOT"]]
+                 [midje "1.5-alpha2"]
+                 [ring "1.1.6"]
+                 [compojure "1.1.3"]
+                 [ring-middleware-format "0.2.2"]]
   :dev-dependencies [[com.stuartsierra/lazytest "1.2.3"]])
