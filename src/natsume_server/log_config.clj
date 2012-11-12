2b3b727a5332e304929e3002a11096c7efdeaa60
Bor Hodošček <bor.hodoscek@gmail.com>
2012-11-12 23:53:28 +0900

    new timbre config helper

---
+++ src/natsume_server/log_config.clj
@@ -0,0 +1,14 @@
+(ns natsume-server.log-config)
+
+;; ## Helper function to configure timbre log
+(defn setup-log
+  [config log-level]
+  (swap! config assoc-in [:appenders :standard-out :enabled?] false)
+  (swap! config assoc-in [:appenders :spit-appender]
+                 {:doc       "Natsume file appender"
+                  :min-level log-level
+                  :enabled?  true
+                  :async?    true
+                  :max-message-per-msecs nil
+                  :fn (fn [{:keys [ap-config level prefix message more] :as args}]
+                        (spit "natsume.log" (format "%s: %s\n" prefix message) :append true))}))
