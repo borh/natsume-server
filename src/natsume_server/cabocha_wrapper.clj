f035562da737147ac5621f5ab0906e71e8c7bc1c
Bor Hodošček <bor.hodoscek@gmail.com>
2013-03-22 16:54:14 +0900

    refactor: cabocha-wrapper ns now only contains the core CaboCha JNI interop code

--- src/natsume_server/cabocha_wrapper.clj
+++ src/natsume_server/cabocha_wrapper.clj
@@ -1,325 +1,84 @@
 (ns natsume-server.cabocha-wrapper
-  (:require [clojure.string :as string]
-            [clojure.java.shell :as shell]
-            [clojure.zip :as z]
-            [taoensso.timbre :as log]
-            [natsume-server.log-config :as lc])
-  (:use
-   [lamina core]
-   [aleph http formats]
-   [clojure.set :only (union)])
-  (:import [com.ibm.icu.text Transliterator Normalizer]))
+  (:require [clojure.string :as string])
+  (:import [org.chasen.cabocha Parser FormatType Token]))
 
-(lc/setup-log log/config :error)
-
-;; With processing of user text we have to take care in repecting the
-;; users preferences, such as the type of periods and commas he/she
-;; prefers, or preferences between full/half-width characters. In
-;; essence, all user input should be returned the way it was input in.
-;; This problem comes up because CaboCha prefers a certain kind of
-;; input, but also because we want to remove any superficial
-;; differences in script use and keep as much data that is
-;; semantically equivalent together.
+;; # Simple CaboCha JNI Wrapper
 ;;
-;; Before sending the input to CaboCha, it should be normalized,
-;; stripped of invalid codepoints, and all half-width characters
-;; should be converted to full-width, etc.
-;; Most importantly all instances of the delimiters `．，` should be
-;; replace with `。、`.
-;; The original string should be kept and preferrably its contents
-;; written back into the CaboCha output morpheme's `orth` field.
+;; The CaboCha JNI bindings should be used per-thread.
+;; The *parser* dynamic var and with-new-parser macro can be used for
+;; concurrent/parallel processing.
+;;
+;; ## TODO
+;;
+;; -   agent pool of parsers? (how to handle locking/race behaviour?)
+(defonce ^:dynamic *parser* (Parser.))
 
-(defn normalize-nfc
+(defmacro with-new-parser [& body]
+  `(binding [*parser* (Parser.)]
+     (do ~@body)))
+
+(def ^:private unidic-features
+  "A vector of all UniDic features, in order, as defined in the UniDic Manual (p. ?) version `2.1.1`."
+  [:pos1 :pos2 :pos3 :pos4 :cType :cForm :lForm :lemma :orth :pron :orthBase :pronBase :goshu :iType :iForm :fType :fForm])
+
+(defn- parse-token [^Token token position surface token-length]
+  (let [features  (string/split (.getFeature token) #",")
+        ne        (.getNe token)
+        token-map (zipmap unidic-features features)]
+    (if (= (count features) 6)
+      (assoc token-map
+        :lemma    surface
+        :orthBase surface
+        :goshu    "不明"
+        :ne       ne
+        :begin    position
+        :end      (+ position token-length))
+      (assoc token-map
+        :ne    ne
+        :begin position
+        :end   (+ position token-length)))))
+
+(defn- parse-tokens [tokens position]
+  (loop [tokens*   tokens
+         position* position
+         parsed    []]
+    (if-let [^Token token (first tokens*)]
+      (let [token-surface (.getSurface token)
+            token-length  (count token-surface)]
+       (recur (rest tokens*)
+              (+ position* token-length)
+              (conj parsed (parse-token token position* token-surface token-length))))
+      parsed)))
+
+(defn parse-sentence
+  "Parses input sentence string and returns a vector of CaboCha chunks containing dependency
+   information and a vector of tokens, which are represented as maps.
+
+   TODO consider deftype/benchmark"
   [^String s]
-  (Normalizer/normalize s Normalizer/NFC))
-
-;; This is apparently very heavyweight, so we should make sure we are
-;; only loadinig it once (I am not sure this is the right way).
-(defonce half-to-fullwidth (Transliterator/getInstance "Halfwidth-Fullwidth"))
-(defn convert-half-to-fullwidth
-  [^String s]
-  (.transliterate half-to-fullwidth s))
-
-;; TODO look into using eda as a supplement to CaboCha
-(defn run-cabocha-on-string
-  "This part is necessitated because I failed to get the Java bindings to link (TODO).
-   The use of numerous config options is because we want to make sure that we are using UniDic for MeCab and then also loading the right CaboCha models."
-  [s]
-  (:out (shell/sh "cabocha"
-                  "-d" "/usr/lib64/mecab/dic/unidic"
-                  "-b" "/usr/lib64/mecab/dic/unidic/dicrc"
-                  "-r" "/etc/cabocharc-unidic"
-                  "-P" "UNIDIC"
-                  "-f1"
-                  :in s)))
-
-;; modified from aleph/test/websocket.clj
-(defmacro with-server [server & body]
-  `(let [kill-fn# ~server]
-     (try
-       ~@body
-       (finally
-         (kill-fn#)))))
-
-(defmacro with-handler [handler & body]
-  `(with-server (start-http-server ~handler {:port 8081 :websocket true})
-     ~@body))
-
-(defn websocket-handler [ch req]
-  (siphon ch req)) ; used to be ch ch
-
-(def cabocha-websocket-channel
-  #(deref (websocket-client {:url "ws://localhost:5010/ws/json"})))
-
-(defn get-cabocha-websocket
-  "For a given string returns a CaboCha analyzed string using WebSockets."
-  [s]
-  (with-handler websocket-handler
-    (let [ch (cabocha-websocket-channel)]
-      (enqueue ch s)
-      (let [r (decode-json @(read-channel ch))]
-        (close ch) ; FIXME understand websockets
-        r))))
-
-(defn tree-to-morphemes-flat
-  [t]
-  (->> t (map :tokens) flatten))
-
-;; ## Tagging middleware
-;;
-;; FIXME what about `名詞＋を＋する／名詞＋に＋なる`?
-
-;; ### Definitions of content and functional word classes
-(def content-word-classes
-  #{:verb :noun :adverb :adjective :preposition})
-
-(def variable-word-classes
-  #{:suffix :prefix :utterance :auxiliary-verb}) ; :symbol?
-
-(def functional-word-classes
-  #{:particle :symbol})
-
-(defn- keywordize-ne
-  [s]
-  (keyword (string/lower-case (string/replace s #"[BI]-" ""))))
-
-(defn unify-aspect-tags
-  "Unifies the separate :aspect and #{:iru :simau :kuru} tags into one :aspect-* tag."
-  [a]
-  (let [match (clojure.set/select #{:iru :simau :kuru} a)]
-    (if (and (:aspect a) (not-empty match))
-      (conj (apply disj a :aspect match)
-            (keyword (str "aspect-" (name (first match)))))
-      a)))
-
-(defn- recode-pos
-  "Recodes tokens in given chunk c into meta-information tags.
-   Word class is recorded in :pos and other tags in :tags set."
-  [c]
-  (reduce
-   (fn [r m]
-     (let [pos (str (:pos1 m) (:pos2 m) (:pos3 m))
-           lemma (:lemma m)
-           pos-code (cond
-                     (re-seq #"^(副詞|名詞.+副詞可能)" pos) :adverb
-                     (re-seq #"^代?名詞[^副]+" pos) :noun
-                     (re-seq #"^連体詞" pos) :preposition
-                     (re-seq #"^動詞" pos) :verb
-                     (and (re-seq #"^助詞接続助詞" pos)
-                          (= "て" lemma)) :auxiliary-verb
-                     (re-seq #"^(助|接続)詞" pos) :particle
-                     (re-seq #"^(形容詞|接尾辞形容詞的)" pos) :adjective
-                     (re-seq #"^((補助)?記号|空白)" pos) :symbol
-                     (and (re-seq #"^助動詞" pos)
-                          (re-seq #"^助動詞-(ダ|デス)$" (:cType m))) :particle
-                     (re-seq #"^助動詞" pos) :auxiliary-verb
-                     (re-seq #"^形状詞" pos) :adjective
-                     (re-seq #"^感動詞" pos) :utterance
-                     (re-seq #"^接頭辞" pos) :prefix
-                     (re-seq #"^接尾辞" pos) :suffix
-                     :else :unknown-pos)
-           tags (cond
-                 (= :verb pos-code) (cond (= "出来る" lemma) #{:dekiru}
-                                          (= "居る"   lemma) #{:iru}
-                                          (= "仕舞う" lemma) #{:simau}
-                                          (= "来る"   lemma) #{:kuru}
-                                          :else #{})
-                 (= :adjective pos-code) (cond (= "無い" lemma) #{:negative} :else #{})
-                 (= :auxiliary-verb pos-code) (cond
-                                               (= (:cType m) "助動詞-タ")                  #{:past}
-                                               (re-seq #"^助動詞-(ナイ|ヌ)$" (:cType m))   #{:negative}
-                                               (re-seq #"^ら?れる$" lemma)                 #{:passive}
-                                               (= (:lemma m) "せる")                       #{:active}
-                                               (re-seq #"^助動詞-(マス|デス)$" (:cType m)) #{:polite}
-                                               (= (:lemma m) "て")                         #{:aspect}
-                                               :else                                       #{})
-                 :else #{})
-           ne (if (not= "O" (:ne m)) #{(keywordize-ne (:ne m))} #{})]
-       (conj r {:pos pos-code :tags (union tags ne)})))
-   []
-   c))
-
-;; ## Transition map
-;;
-;; Defines when POS should change or not.
-
-(defn- vector-map->map
-  "Helper function to make a map from map-like vectors."
-  [v]
-  (apply merge (map #(apply hash-map %) v)))
-
-(def transitions
-  (merge {[:verb :noun] :verb
-          [:adjective :noun] :adjective
-          [:adjective :verb] :adjective
-          [:verb :adjective] :verb
-          [:auxiliary-verb :verb] :verb
-          [:auxiliary-verb :adjective] :adjective
-          [:suffix :noun] :noun
-          [:noun :symbol] :noun}
-         (vector-map->map
-          (for [c content-word-classes f functional-word-classes] [[c f] f]))
-         (vector-map->map
-          (for [pos (union content-word-classes functional-word-classes variable-word-classes)]
-            [[pos pos] pos]))))
-
-(defn- enumerate
-  "Enumerates (indexes) given sequence."
-  [s]
-  (map-indexed vector s))
-
-(defn- update-if-not-nil
-  [m k v]
-  (let [curr-v (get m k)]
-    (if (nil? (curr-v))
-      v
-      curr-v)))
-
-(defn- reduce-with-transitions
-  [indexed-tokens]
-  (log/trace (format "START REDUCE: '%s'" indexed-tokens))
-  (reduce
-   (fn [accum i-t]
-     (log/trace (format "  accum: '%s'\ti-t: '%s'" accum i-t))
-     (let [get-pos #(:pos (second %))
-           get-tags #(:tags (second %))
-           i (first i-t)
-           pos (get-pos i-t)
-           tags (get-tags i-t)
-           new-t {:pos (get transitions [(get-pos (peek accum)) pos]) :tags (union tags (get-tags (peek accum)))}] ; 1.
-       (log/trace
-        (format "    new-t: '%s'; input: '%s'" new-t [(get-pos (peek accum)) pos]))
-       (if (nil? (:pos new-t))
-         ;; Replace token:
-         (do (log/trace (format "    not replacing %s" i-t))    (conj accum i-t)) ; Add token
-         (do (log/trace (format "    replacing with %s" new-t)) (conj (pop accum) [i (union (get-tags (pop accum)) new-t)]))))) ; Replace token
-   []
-   indexed-tokens))
-
-(defn- make-composite-token-string
-  "Given a chunk and begin and end token indexes, compounds the orthographic representations of
-   tokens into bigger units in a smart way.
-
-   For example, given 続け + られ + た, it will return 続けられる."
-  [begin end c]
-  ;;(println begin)
-  ;;(println end)
-  ;;(println (vec c))
-  ;;(println "hello")
-  ;;(println (map #(get-in (vec c) [%]) (range begin end)))
-  ;;(println (:orthBase (last (map #(get-in (vec c) [%]) (range begin end)))))
-  ;;(println (vec (map :orth (pop (map #(get-in (vec c) [%]) (range begin end))))))
-  (let [tokens (vec (map #(get-in (vec c) [%]) (range begin end)))
-        smart-base #(if (re-seq #"^助動?詞" (:pos1 %)) (:orth %) (:orthBase %)) ; :lemma is another possibility
-        tail-token-orthbase (smart-base (peek tokens))
-        head-tokens-orth    (vec (map :orth (pop tokens)))]
-    (log/trace (format "begin: '%s' end: '%s' head-token-orthbase: '%s' tail-token-orthbase: '%s'" begin end head-tokens-orth tail-token-orthbase))
-    (apply str (conj head-tokens-orth tail-token-orthbase))))
-
-(defn- infer-type-chunk
-  "Infers the content and functional parts of the chunk in reverse order.
-   Also corrects the :head and :tail indexes given by CaboCha, which are sometimes wrong.
-
-   1. Apply transition on reversed token sequence.
-      When two tokens match, they are recombined into the transitioned one.
-      The lesser token index is kept.
-   2. Repeat 1. until no matches are found.
-   3. Assign :tail and :tail-pos if functional token exists, likewise for content token.
-
-   All combinations of the three chunk types are possible: content, function, and modality.
-
-   TODO: In reality, the functional part can (and should) be split into two possible parts.
-         Example: 言ったかも知れないが = 言った=noun + かも知れない=functional_1 + が=functional_2
-         In this example, functional_1 would be modality, while funcitonal_2 would be normal case particle"
-  [c]
-  (let [indexed-tokens (reverse (enumerate (recode-pos c)))
-        maybe-head-tail (loop [trans-i-tokens indexed-tokens] ; 2.
-                          (log/trace (format "LOOP: '%s'" trans-i-tokens))
-                          (let [reduced (reduce-with-transitions trans-i-tokens)]
-                            (if (not= trans-i-tokens reduced)
-                              (recur reduced)
-                              trans-i-tokens)))]
-    (second
-     (reduce ; 3.
-      (fn [[l m] [i t]] ; l = last index, m = map
-        (let [pos (:pos t)
-              tags (unify-aspect-tags (:tags t))
-              base-string (make-composite-token-string i (if (nil? l) (count c) l) c)]
-          (if (functional-word-classes pos)
-            [i (assoc m :tail-pos pos :tail-tags tags :tail i :tail-string base-string)]
-            [i (assoc m :head-pos pos :head-tags tags :head i :head-string base-string)])))
-      [nil
-       {:head-pos nil :head-tags nil :tail-pos nil :tail-string nil
-        :head     nil :tail-tags nil :tail     nil :head-string nil}]
-      maybe-head-tail))))
-
-(defn- annotate-chunk
-  [c]
-  (merge c (infer-type-chunk (:tokens c))))
-
-(defn- annotate-tree
-  [t]
-  (map annotate-chunk t))
-
-(defn- revert-orth-with
-  "Reverts the :orth of all tokens in tree to their pre-NFC norm form."
-  [tree original-input]
-  (log/trace (format "tree: '%s'\noriginal-input: '%s'" (apply str (map :orth (tree-to-morphemes-flat tree))) original-input))
-  (let [update-morpheme #(assoc % :orth (subs original-input (:begin %) (:end %)))
-        update-morphemes #(map update-morpheme %)
-        update-chunk #(update-in % [:tokens] update-morphemes)]
-    (map update-chunk tree)))
-
-;; Pre-processing (normalization, common substitutions, etc.) is done on the string before it is processed by CaboCha.
-;; The whole process is as follows:
-;;
-;; 1. save original string for later step
-;; 2. do Unicode NFC on the input string
-;; 3. substitute all occurrences of `．，` with `。、`, and half- with full-width characters
-;; 4. send resulting string to CaboCha
-;; 5. tag all chunks by chunk type (noun phrase, adjectival phrase etc. by scanning head-tail information (:head-pos = :noun, :tail-pos :p-ga, etc.)
-;; 5. join certain classes of chunks like `プレゼントをする`, i.e. `名詞＋を＋する／名詞＋に＋なる`
-;; 6. replace the `orth` fields of all morphemes in CaboCha output with characters in the original string
-(defn sentence->tree
-  "Converts string into CaboCha tree data structure."
-  [s]
-  (-> s
-      normalize-nfc              ;2
-      convert-half-to-fullwidth  ;2
-      (string/replace "．" "。") ;3
-      (string/replace "，" "、") ;3
-      ;;run-cabocha-on-string    ;4
-      ;;parse-cabocha-format     ;4
-      get-cabocha-websocket      ;4 ;; faster until we get JNI bindings working
-      (revert-orth-with s)       ;6
-      annotate-tree))            ;5
-
-;; # TODO
-;;
-;; - is there any way to parse CaboCha input with zippers?
-;;
-;;   Doing it with zippers should clean up the code and make it possible to have real 'nested' type structures.
-;;   After more consideration, there is no way to make a tree structure represent the dual-linked (adjacency and dependency link) graph structure of CaboCha dependency grammar trees, short of making it recurive in nature/making custom tree traversal code.
-;;   Therefore making separate custom traversal code for both adjacency and link structure makes more sense (at the expense of having two ways of traversing trees instead of one -- which is unavoidable anyway?).
-;;   Zippers can still be used to construct the initial structure.
-;;   We should use vectors to support `nth` type access with O(1) costs.
+  (let [tree (.parse ^Parser *parser* s)
+        chunks (.chunk_size tree)]
+    (loop [chunk-id 0
+           token-id 0
+           position 0
+           parsed   []]
+      (if (< chunk-id chunks)
+       (let [chunk   (.chunk tree chunk-id)
+             link-id (.getLink chunk)
+             prob    (.getScore chunk)
+             head    (.getHead_pos chunk)
+             tail    (.getFunc_pos chunk)
+             token-count (.getToken_size chunk)
+             token-list  (mapv #(.token tree %) (range token-id (+ token-id token-count)))
+             tokens      (parse-tokens token-list position)]
+         (recur
+          (inc chunk-id)
+          (+ token-id token-count)
+          (int (:end (last tokens)))
+          (conj parsed {:id chunk-id
+                        :link link-id
+                        :head head
+                        :tail tail
+                        :prob prob
+                        :tokens tokens})))
+       parsed))))
