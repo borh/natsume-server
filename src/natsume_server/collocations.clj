da08210daf589dc659138bca99a297b7204dd825
Bor Hodošček <bor.hodoscek@gmail.com>
2013-03-22 17:02:07 +0900

    collocation related functionality in new ns

--- src/natsume_server/collocations.clj
+++ src/natsume_server/collocations.clj
@@ -1,8 +1,54 @@
 (ns natsume-server.collocations
-  (:require [natsume-server.cabocha-wrapper :as cw]))
+  (:require [clojure.string :as string]
+            [taoensso.timbre :as log]
+            [natsume-server.log-config :as lc])
+  (:import [com.ibm.icu.text Transliterator Normalizer]))
 
-;; :pos1 形容詞 -> :display :t, :type :adjective
-;; :pos1 助動詞 -> :display :f, :type :adjective
+(lc/setup-log log/config :trace)
 
-;; The final form recorded for insertion into Natsume is filtered for
-;; :display, and the :lemma form is used(?)
+;; Collocation extraction
+
+(defn get-target [t source target]
+  ;;(println "getting..." source target t)
+  (cond (= source (dec (count t))) nil
+        (= -1 target) (nth t (dec (count t)))
+        :else (nth t target)))
+
+(defn keyword-combine [& ks]
+  (keyword (string/join "-" (map name ks))))
+
+(defn extract-head-tail-keys [m]
+  (select-keys m [:head-string :head-pos :head-tags :head-begin :head-end
+                  :tail-string :tail-pos :tail-tags :tail-begin :tail-end]))
+
+(defn detect-collocation
+  "TODO only picks up tuples or triplets, should expand to arbitrary lengths (when it makes sense to).
+   FIXME doesn't correctly handle the case of multple occurences of the same POS (kind of fixed).
+   FIXME generalize to operate on chunk seq and filter for known good combinations
+   FIXME FIXME FIXME rather, keep the order of chunks by putting them in a vector, as it helps with ambiguity; also make it more generic -- waaay to complicated now for what should be relatively simple -- the reason is that we are trying to fit this into the SQL/korma insert module."
+  [a b]
+  ;;(println "detecting..." a b)
+  (let [head-keys [:head-string :head-pos :head-tags :head-begin :head-end]
+        tail-keys [:tail-string :tail-pos :tail-tags :tail-begin :tail-end]
+        type-data (filterv #(every? (complement nil?) (vals %))
+                           [(select-keys a head-keys)
+                            (select-keys a tail-keys)
+                            (select-keys b head-keys)
+                            (select-keys b tail-keys)])]
+    {:type (mapv #(or (:head-pos %) (:tail-pos %)) type-data)
+     :data type-data}))
+
+(defn extract-collocations [tree]
+  (reduce
+   (fn [coll chunk]
+     (let [source-id (:id chunk)
+           target-id (:link chunk)
+           target-chunk (get-target tree source-id target-id)]
+       ;;(println "extracting" source-id target-id chunk target-chunk)
+       (if target-chunk
+         (if-let [collocation (detect-collocation (extract-head-tail-keys chunk)
+                                                  (extract-head-tail-keys target-chunk))]
+           (conj coll collocation))
+         coll)))
+   []
+   tree))
