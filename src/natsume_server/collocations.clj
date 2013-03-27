ef03edfaca005b5bd79fd9f43474bb2a3ed00d6c
Bor Hodošček <bor.hodoscek@gmail.com>
2013-03-27 10:49:37 +0900

    cleanups: private functions and logging

--- src/natsume_server/collocations.clj
+++ src/natsume_server/collocations.clj
@@ -1,33 +1,27 @@
 (ns natsume-server.collocations
-  (:require [clojure.string :as string]
-            [taoensso.timbre :as log]
-            [natsume-server.log-config :as lc])
+  (:require [clojure.string :as string])
   (:import [com.ibm.icu.text Transliterator Normalizer]))
 
-(lc/setup-log log/config :trace)
+;; ## Collocation extraction
 
-;; Collocation extraction
-
-(defn get-target [t source target]
-  ;;(println "getting..." source target t)
+(defn- get-target [t source target]
   (cond (= source (dec (count t))) nil
         (= -1 target) (nth t (dec (count t)))
         :else (nth t target)))
 
-(defn keyword-combine [& ks]
+(defn- keyword-combine [& ks]
   (keyword (string/join "-" (map name ks))))
 
-(defn extract-head-tail-keys [m]
+(defn- extract-head-tail-keys [m]
   (select-keys m [:head-string :head-pos :head-tags :head-begin :head-end
                   :tail-string :tail-pos :tail-tags :tail-begin :tail-end]))
 
-(defn detect-collocation
+(defn- detect-collocation
   "TODO only picks up tuples or triplets, should expand to arbitrary lengths (when it makes sense to).
    FIXME doesn't correctly handle the case of multple occurences of the same POS (kind of fixed).
    FIXME generalize to operate on chunk seq and filter for known good combinations
    FIXME FIXME FIXME rather, keep the order of chunks by putting them in a vector, as it helps with ambiguity; also make it more generic -- waaay to complicated now for what should be relatively simple -- the reason is that we are trying to fit this into the SQL/korma insert module."
   [a b]
-  ;;(println "detecting..." a b)
   (let [head-keys [:head-string :head-pos :head-tags :head-begin :head-end]
         tail-keys [:tail-string :tail-pos :tail-tags :tail-begin :tail-end]
         type-data (filterv #(every? (complement nil?) (vals %))
@@ -44,7 +38,6 @@
      (let [source-id (:id chunk)
            target-id (:link chunk)
            target-chunk (get-target tree source-id target-id)]
-       ;;(println "extracting" source-id target-id chunk target-chunk)
        (if target-chunk
          (if-let [collocation (detect-collocation (extract-head-tail-keys chunk)
                                                   (extract-head-tail-keys target-chunk))]
