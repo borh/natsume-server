e453674b00dc5bb4e0bfd6c4526d5f478d31ffdb
Bor Hodošček <bor.hodoscek@gmail.com>
2013-04-05 15:19:47 +0900

    add optional log name param

--- src/natsume_server/log_config.clj
+++ src/natsume_server/log_config.clj
@@ -2,17 +2,19 @@
 
 ;; ## Helper function to configure timbre log
 (defn setup-log
-  [config log-level]
-  (swap! config assoc-in [:timestamp-pattern] "HH:mm:ss.SSS")
-  (swap! config assoc-in [:prefix-fn]
-         (fn [{:keys [level timestamp hostname ns]}]
-           (str timestamp " " (name level) " [" ns "]")))
-  (swap! config assoc-in [:appenders :standard-out :enabled?] false)
-  (swap! config assoc-in [:appenders :spit-appender]
-                 {:doc       "Natsume file appender"
-                  :min-level log-level
-                  :enabled?  true
-                  :async?    true
-                  :max-message-per-msecs nil
-                  :fn (fn [{:keys [ap-config level prefix message more] :as args}]
-                        (spit "log/natsume.log" (format "%s: %s\n" prefix message) :append true))}))
+  ([config log-level]
+     (setup-log config log-level "natsume"))
+  ([config log-level log-name]
+     (swap! config assoc-in [:timestamp-pattern] "HH:mm:ss.SSS")
+     (swap! config assoc-in [:prefix-fn]
+            (fn [{:keys [level timestamp hostname ns]}]
+              (str timestamp " " (name level) " [" ns "]")))
+     (swap! config assoc-in [:appenders :standard-out :enabled?] false)
+     (swap! config assoc-in [:appenders :spit-appender]
+            {:doc       "Natsume file appender"
+             :min-level log-level
+             :enabled?  true
+             :async?    true
+             :max-message-per-msecs nil
+             :fn (fn [{:keys [ap-config level prefix message more] :as args}]
+                   (spit (str "log/" log-name ".log") (format "%s: %s\n" prefix message) :append true))})))
