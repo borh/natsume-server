f343eb73a1688a2f0ae415099a4ef69569d3e1c2
Bor Hodošček <bor.hodoscek@gmail.com>
2013-05-06 11:12:05 +0900

    new config ns
    More fine-grained and safe (with validation) than the previous.

---
+++ src/natsume_server/config.clj
@@ -0,0 +1,81 @@
+(ns natsume-server.config
+  (:require [cfg.core :refer :all]))
+
+(init)
+
+(defopt :log-directory
+  :default  "./log"
+  :validate #(.isFile (clojure.java.io/file %))
+  :help-string "Set logging directory"
+  :aliases  ["l" "-log-directory"])
+
+(defopt :verbose
+  :default false
+  :bool    true
+  :help-string "Turn on verbose logging"
+  :aliases ["v" "-verbose"])
+
+(defopt :clean
+  :default false
+  :bool    true
+  :help-string "Reset database on run (WARNING: will delete all data)"
+  :aliases ["c" "-clean"])
+
+(defopt :no-process
+  :default  false
+  :bool     true
+  :help-string "Do not process corpus data (processes by default)"
+  :aliases  ["-no-process"])
+
+(defopt :search
+  :default true
+  :bool    true
+  :help-string "Create search-optimized tables"
+  :aliases ["-search"])
+
+(defopts :sampling
+  (defopt :ratio
+    :default  0.0
+    :validate #(and (<= % 1.0) (>= % 0.0))
+    :parse    #(Double/parseDouble %)
+    :help-string "Sample size (as percent of original population (0.0-1.0))"
+    :aliases  ["-sampling-ratio"])
+  (defopt :seed
+    :default  2
+    :validate integer?
+    :help-string "Random number seed for sampling"
+    :aliases  ["-sampling-seed"])
+  (defopt :replace
+    :default false
+    :bool    true
+    :help-string "Use replacement with sampling"
+    :aliases ["-sampling-replace"])
+  (defopt :hold-out
+    :default false
+    :bool    true
+    :help-string "FIXME"
+    :aliases ["-sampling-hold-out"]))
+
+(defopts :models
+  (defopt :n-gram
+    :default false
+    :bool    true
+    :aliases ["-n-gram"])
+  (defopt :directory
+    :default  "./data/models/"
+    :validate #(.isFile (clojure.java.io/file %))
+    :help-string "Set model persistence directory"
+    :aliases  ["-models-directory"]))
+
+(defopts :server
+  (defopt :run
+    :default false
+    :bool    true
+    :help-string "Run server"
+    :aliases ["s" "-server"])
+  (defopt :port
+    :default  3000
+    :parse    #(Integer/parseInt %)
+    :validate #(and (integer? %) (pos? %))
+    :help-string "Server port"
+    :aliases  ["p" "-port"]))
