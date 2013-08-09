13b39e60c6dd7f82a674a09058a09a2777f2a34d
Bor Hodošček <bor.hodoscek@gmail.com>
2013-08-09 12:55:53 +0900

    updates to CaboCha wrapper: fix to single thread

--- src/natsume_server/cabocha_wrapper.clj
+++ src/natsume_server/cabocha_wrapper.clj
@@ -1,44 +1,33 @@
 (ns natsume-server.cabocha-wrapper
   (:require [clojure.string :as string]
+            [flatland.useful.utils :as utils]
+            [plumbing.core :refer [?>]]
             [qbits.knit :as knit])
   (:import [org.chasen.cabocha Parser FormatType Token]))
 
 ;; # Simple CaboCha JNI Wrapper
 ;;
-;; The CaboCha JNI bindings should be used per-thread.
-;; The *parser* dynamic var and with-new-parser macro can be used for
-;; concurrent/parallel processing.
-;;
-;; ## TODO
-;;
-;; -   agent pool of parsers? (how to handle locking/race behaviour?)
-(defonce ^:dynamic *parser* (Parser.))
+;; The CaboCha JNI bindings are not thread-safe, so the parser is wrapped in flatland.useful.utils' thread-local macro.
 
-(defmacro with-new-parser [& body]
-  `(binding [*parser* (Parser.)]
-     (do ~@body)))
+(defonce parser (utils/thread-local (Parser.)))
 
 (def ^:private unidic-features
   "A vector of all UniDic features, in order, as defined in the UniDic Manual (p. ?) version `2.1.1`."
-  [:pos1 :pos2 :pos3 :pos4 :cType :cForm :lForm :lemma :orth :pron :orthBase :pronBase :goshu :iType :iForm :fType :fForm])
+  [:pos-1 :pos-2 :pos-3 :pos-4 :c-type :c-form :l-form :lemma :orth :pron :orth-base :pron-base :goshu :i-type :i-form :f-type :f-form])
 
 (defn- parse-token [^Token token position surface token-length]
   (let [features  (string/split (.getFeature token) #",")
         ne        (.getNe token)
         token-map (zipmap unidic-features features)]
-    (if (= (count features) 6)
-      (assoc token-map
-        :lemma    surface
-        :orthBase surface
-        :pronBase surface ; FIXME
-        :goshu    "不明"
-        :ne       ne
-        :begin    position
-        :end      (+ position token-length))
-      (assoc token-map
-        :ne    ne
-        :begin position
-        :end   (+ position token-length)))))
+    (-> token-map
+        (assoc :ne    ne
+               :begin position
+               :end   (+ position token-length))
+        (?> (= (count features) 6) assoc
+            :lemma     surface
+            :orth-base surface
+            :pron-base surface ; FIXME translate to katakana.
+            :goshu     "不明"))))
 
 (defn- parse-tokens [tokens position]
   (loop [tokens*   tokens
@@ -58,7 +47,7 @@
 
    TODO consider deftype/benchmark"
   [^String s]
-  (let [tree   (.parse ^Parser *parser* s)
+  (let [tree   (.parse ^Parser @parser s)
         chunks (.chunk_size tree)]
     (loop [chunk-id 0
            token-id 0
@@ -92,13 +81,11 @@
   [thread-group]
   (reify java.util.concurrent.ThreadFactory
     (newThread [_ f]
-      (with-new-parser
-        (doto (Thread. ^ThreadGroup thread-group ^Runnable f)
-          (.setDaemon true))))))
+      (doto (Thread. ^ThreadGroup thread-group ^Runnable f)
+        (.setDaemon true)))))
 (def cabocha-executor
-  (knit/executor :single
-                 ;;:fixed ; -> this deadlocks
-                 ;;:num-threads 6
+  (knit/executor :fixed
+                 :num-threads (.. Runtime getRuntime availableProcessors)
                  :thread-factory (cabocha-thread-factory (knit/thread-group "cabocha-thread"))))
 
 (defn parse-sentence-synchronized [^String s]
