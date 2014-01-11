467fbcf78dbc035819048d65cb01b8846aa7ec9a
Bor Hodošček <bor.hodoscek@gmail.com>
2014-01-11 11:02:50 +0900

    refresh pedestal setup (needs more work)

--- src/natsume_server/api/main.clj
+++ src/natsume_server/api/main.clj
@@ -1,34 +1,62 @@
 (ns natsume-server.api.main
   (:require [natsume-server.config :as cfg]
 
+            [io.pedestal.service-tools.server :as server]
             [natsume-server.api.service :as service]
+            [io.pedestal.service-tools.dev :as dev]
             [io.pedestal.service.http :as bootstrap]))
 
-(def service-instance
-  "Global var to hold service instance."
-  nil)
+(defn run-dev
+  "The entry-point for 'lein run-dev'"
+  [& args]
+  (dev/init service/service #'service/routes)
+  (apply dev/-main args))
 
-(defn create-server
-  "Standalone dev/prod mode."
-  [& [opts]]
-  (alter-var-root #'service-instance
-                  (constantly (bootstrap/create-server (merge service/service opts)))))
+;; To implement your own server, copy io.pedestal.service-tools.server and
+;; customize it.
+;;;;(def service-instance
+;;;;  "Global var to hold service instance."
+;;;;  nil)
 
-;; Replacing -main
-(defn start-server! [& args]
-  (create-server)
-  (bootstrap/start service-instance))
+(defn start-server!
+  "The entry-point for 'lein run'"
+  [& args]
+  (server/init service/service)
+  (apply server/-main args))
 
-;; Container prod mode for use with the io.pedestal.servlet.ClojureVarServlet class.
+;; Fns for use with io.pedestal.servlet.ClojureVarServlet
 
 (defn servlet-init [this config]
-  (alter-var-root #'service-instance
-                  (constantly (bootstrap/create-servlet service/service)))
-  (.init (::bootstrap/servlet service-instance) config))
+  (server/init service/service)
+  (server/servlet-init this config))
 
 (defn servlet-destroy [this]
-  (alter-var-root #'service-instance nil))
+  (server/servlet-destroy this))
 
 (defn servlet-service [this servlet-req servlet-resp]
-  (.service ^javax.servlet.Servlet (::bootstrap/servlet service-instance)
-            servlet-req servlet-resp))
+  (server/servlet-service this servlet-req servlet-resp))
+
+;;(defn create-server
+;;  "Standalone dev/prod mode."
+;;  [& [opts]]
+;;  (alter-var-root #'service-instance
+;;                  (constantly (bootstrap/create-server (merge service/service opts)))))
+;;
+;;;; Replacing -main
+;;(defn start-server! [& args]
+;;  (create-server)
+;;  (bootstrap/start service-instance))
+;;
+;;;; Container prod mode for use with the io.pedestal.servlet.ClojureVarServlet class.
+;;
+;;(defn servlet-init [this config]
+;;  (alter-var-root #'service-instance
+;;                  (constantly (bootstrap/create-servlet service/service)))
+;;  (.init (::bootstrap/servlet service-instance) config))
+;;
+;;(defn servlet-destroy [this]
+;;  (alter-var-root #'service-instance nil))
+;;
+;;(defn servlet-service [this servlet-req servlet-resp]
+;;  (.service ^javax.servlet.Servlet (::bootstrap/servlet service-instance)
+;;            servlet-req servlet-resp))
