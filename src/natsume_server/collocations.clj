d193701e775d828068d1b6a8797500073d4bebf8
Bor Hodošček <bor.hodoscek@gmail.com>
2014-01-17 16:48:11 +0900

    cleanup and add temporary fix for duplication of results

--- src/natsume_server/collocations.clj
+++ src/natsume_server/collocations.clj
@@ -12,30 +12,8 @@
   ([t coll]
      (get-target t (:id coll) (:link coll))))
 
-(comment
-  (defn- keyword-combine [& ks]
-    (keyword (string/join "-" (map name ks)))))
-
 (def head-keys [:head-string :head-pos :head-tags :head-begin :head-end])
 (def tail-keys [:tail-string :tail-pos :tail-tags :tail-begin :tail-end])
-
-(comment
-  (defn- extract-head-tail-keys [m]
-    (select-keys m (concat head-keys tail-keys)))
-
-  (defn- detect-collocation
-    "TODO only picks up tuples or triplets, should expand to arbitrary lengths (when it makes sense to).
-   FIXME doesn't correctly handle the case of multiple occurences of the same POS (kind of fixed).
-   FIXME generalize to operate on chunk seq and filter for known good combinations
-   FIXME FIXME FIXME rather, keep the order of chunks by putting them in a vector, as it helps with ambiguity; also make it more generic -- waaay to complicated now for what should be relatively simple -- the reason is that we are trying to fit this into the SQL/korma insert module."
-    [a b]
-    (let [type-data (filterv #(every? (complement nil?) (vals %))
-                             [(select-keys a head-keys)
-                              (select-keys a tail-keys)
-                              (select-keys b head-keys)
-                              (select-keys b tail-keys)])]
-      {:type (mapv #(or (:head-pos %) (:tail-pos %)) type-data)
-       :data type-data})))
 
 (defn- follow-link
   "Given a tree (coll), a chunk in that tree, and a path through the tree, recurs until the end of the link path and returns it."
@@ -49,12 +27,6 @@
         (recur target
                (conj path* target))
         path*))))
-
-(comment
-  (defn- quality-check [path]
-    (->> path
-         (r/take-while (fn [chunk] (every? (complement nil?) (vals (select-keys chunk (concat head-keys tail-keys))))))
-         (into []))))
 
 (defn- make-collocation [path]
   {:type (mapv #(or (:head-pos %) (:tail-pos %)) path)
@@ -78,20 +50,6 @@
                             (for [n (range (let [order (count path)]
                                              (if (> order max-order) max-order order)))]
                               (make-collocation (subvec path 0 (inc n))))))
-                    [])))
+                    [])
+          distinct)) ;; FIXME: そうですね。
   ([tree] (extract-collocations tree 4)))
-
-(comment
-  (defn extract-collocations [tree]
-    (reduce
-     (fn [coll chunk]
-       (let [source-id (:id chunk)
-             target-id (:link chunk)
-             target-chunk (get-target tree source-id target-id)]
-         (if target-chunk
-           (if-let [collocation (detect-collocation (extract-head-tail-keys chunk)
-                                                    (extract-head-tail-keys target-chunk))]
-             (conj coll collocation))
-           coll)))
-     []
-     tree)))
