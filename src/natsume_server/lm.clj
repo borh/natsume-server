c83b3dbaeb24e192a193badfe852a9fb4903d020
Bor Hodošček <bor.hodoscek@gmail.com>
2014-01-19 10:25:58 +0900

    refactor and expand lm to new tree zipper model; new similarity functions

--- src/natsume_server/lm.clj
+++ src/natsume_server/lm.clj
@@ -3,9 +3,13 @@
             [clojure.java.io :as io]
             [me.raynes.fs :as fs]
             [plumbing.core :refer :all]
+            [incanter.stats :as stats]
             [natsume-server.models.db :as db]
+            [natsume-server.models.tree :as tree]
+            [natsume-server.utils.naming :as naming]
             [natsume-server.config :as cfg])
-  (:import [com.aliasi.lm LanguageModel NGramProcessLM CompiledNGramProcessLM #_LanguageModel$Tokenized]
+  (:import [com.aliasi.lm LanguageModel NGramProcessLM CompiledNGramProcessLM TokenizedLM #_LanguageModel$Tokenized]
+           [com.aliasi.tokenizer RegExTokenizerFactory]
            [java.io ObjectOutputStream FileOutputStream ObjectInputStream FileInputStream]))
 
 ;; TODO lazy-loading (and unloading) of already compiled models
@@ -24,13 +28,19 @@
       :or {n 3 num-chars 4000 lambda-factor 6.0}}]
   (NGramProcessLM. n num-chars lambda-factor))
 
+(defn build-token-model!
+  [& {:keys [n lambda-factor]
+      :or {n 3 lambda-factor 6.0}}]
+  (let [factory (RegExTokenizerFactory. "\\S+")]
+    (TokenizedLM. factory n)))
+
 (defn train!
   "Add text to n-gram model."
-  [^NGramProcessLM model text]
-  (.handle model text))
+  [model text] ;; ^NGramProcessLM
+  (.handle model text)) ; Or `.train`.
 
 (defn eval-on-model
-  [^CompiledNGramProcessLM model text & {:keys [perplexity]}] ;; Smaller perplexity means better model.
+  [model text & {:keys [perplexity]}] ;; Smaller perplexity means better model. ; ^CompiledNGramProcessLM
   (let [log2estimate (if-not (empty? text)
                        (/ (.log2Estimate model text)
                           (count text))
@@ -44,66 +54,22 @@
 ;; FIXME move from level/name model to X.Y.Z.K? filename -- ie. just the tree-map, maybe even using the tree function in models.db
 
 (defn compile-model! [model-name model]
-  (with-open [out (-> (str "data/models/" model-name ".comp.lm.model")
+  (with-open [out (-> (str "data/models/" model-name ".model")
                       io/file
                       FileOutputStream.
                       ObjectOutputStream.)]
-    (.compileTo ^NGramProcessLM model out)))
+    (.compileTo model out))) ;; ^NGramProcessLM
 
 (defn deserialize-model!
   ([fn] (with-open [in (-> fn
                            io/file
                            FileInputStream.
                            ObjectInputStream.)]
-          (.readObject in)))
-  ([model-name compiled?]
-     (deserialize-model! (str "data/models/" model-name (if compiled? ".comp") ".lm.model"))))
+          (.readObject in))))
 
 ;; TODO move to new ltree model (need to replace most of the bunk save/load functions)
 ;; TODO need a way to remove duplicates: X.Y.Z.K + X.Y.Z -> X.Y.Z.K where K is the only child of X.Y.Z
 ;; Only run once all models have been made, and only when needed.
-
-(defonce all-models
-  (let [model-files (fs/glob "data/models/*.model")
-        model-names (->> model-files
-                         (map #(-> %
-                                   fs/base-name
-                                   (string/replace #"\.(model|comp|lm)" "")))
-                         (map #(string/split % #"\.")))]
-    (zipmap model-names (map deserialize-model! model-files))))
-
-(defn eval-on-all-models [text]
-  (println (count text))
-  (let [ks (keys all-models)]
-    (zipmap ks (pmap #(eval-on-model (get all-models %) text :perplexity true)
-                     ks))))
-
-(defn get-genre-lm-similarities []
-  (let [genres (db/get-genres)]
-    (for-map [source-genre genres]
-             source-genre (->> source-genre
-                               db/sources-ids-by-genre
-                               (map db/sources-text)
-                               (apply str)
-                               eval-on-all-models
-                               #_(sort-by second)))))
-
-
-(let [fn (fs/normalized-path "data/computed-lm-similarities.clj")]
-  (if (cfg/opt :models :n-gram)
-    (if (fs/exists? fn)
-      (defonce genre-lm-similarities (map-vals #(sort-by first %) (read-string (slurp fn))))
-      (defonce genre-lm-similarities (get-genre-lm-similarities))) ; WARNING computationally expensive, should persist.
-    (def genre-lm-similarities)))
-
-(defn save-similarity-table! [sims]
-  (spit "data/computed-lm-similarities.clj" (pr-str sims))
-  #_(with-open [out-file (io/writer "lm-similarity.tvs")]
-      (let [header (mapcat keys (vals sims))]
-        (clojure.data.csv/write-csv out-file [(mapv #(string/join "." %) header)] :separator \tab) ; header
-        (doseq [line (mapv vals (vals sims))]
-          (clojure.data.csv/write-csv out-file [line] :separator \tab)))))
-
 (defn- expand-seq [sequence]
   (loop [r  '()
          xs (reverse sequence)]
@@ -111,30 +77,166 @@
       (recur (conj r (reverse xs))
              (next xs))
       r)))
+(defn- expand-seq-distinct [sequence]
+  (distinct (map vec (mapcat expand-seq sequence))))
 
-(defn compile-all-models! []
-  (let [genres (distinct (map vec (mapcat expand-seq (db/get-genres))))]
+(defonce all-models
+  ;; FIXME type and n as parameter
+  (let [model-files (fs/glob "data/models/*.model")
+        model-names (->> model-files
+                         (map #(-> %
+                                   fs/base-name
+                                   (string/replace #"-.+" "")))
+                         (map #(string/split % #"\.")))]
+    (zipmap model-names (map deserialize-model! model-files))))
+
+;; FIXME we are making too many models, ex.: parents with only one child are same data!
+(defn eval-on-all-models [text]
+  (println (count text))
+  (let [ks (keys all-models)]
+    (zipmap ks (pmap #(let [m (get all-models %)]
+                        (->> text
+                             (map (fn [line] (eval-on-model m line :perplexity true)))
+                             incanter.stats/mean))
+                     ks))))
+
+(defn save-similarity-table! [sims & {:keys [type n] :or {type :char n 3}}]
+  (spit (str "data/" (name type) "-" n "-computed-lm-similarities.clj") (pr-str sims))
+  #_(with-open [out-file (io/writer "lm-similarity.tsv")]
+      (let [header (mapcat keys (vals sims))]
+        (clojure.data.csv/write-csv out-file [(mapv #(string/join "." %) header)] :separator \tab) ; header
+        (doseq [line (mapv vals (vals sims))]
+          (clojure.data.csv/write-csv out-file [line] :separator \tab)))))
+
+(defn get-genre-lm-similarities [& {:keys [type] :or {type :char}}]
+  (let [genres (expand-seq-distinct (map natsume-server.models.sql-helpers/ltree->seq (db/get-genres)))]
+    (for-map [source-genre genres]
+        source-genre (->> source-genre
+                          db/sources-ids-by-genre
+                          (mapcat (case type :char db/sources-text :token db/sources-tokens))
+                          eval-on-all-models))))
+
+(defn- parse-fn [fn]
+  (let [match (re-seq #"^(.+)-(\d)" (fs/base-name fn))
+        [type n] (-> match first rest)]
+    [(keyword type) (Integer/parseInt n)]))
+;; TODO init function
+(case (cfg/opt :models :mode)
+  :noop  (declare genre-lm-similarities) ; To fix compile.
+  :build (defonce genre-lm-similarities (get-genre-lm-similarities)) ; WARNING computationally expensive, should persist.
+  :load  (if-let [fn (fs/normalized-path (format "data/%s-%d-computed-lm-similarities.clj"
+                                                 (name (cfg/opt :models :n-gram :type))
+                                                 (cfg/opt :models :n-gram :n)))]
+           (defonce genre-lm-similarities (read-string (slurp fn)))))
+
+(defn- train-char-model [model model-name genre n & {:keys [full-model]}]
+  (->> genre
+       db/sentences-by-genre
+       ((comp dorun map) #(train! model %)))
+  (compile-model! model-name model)
+  #_(let [model-name (str (string/join "." genre) "-char-" n "-gram")
+          model (build-char-model!)]))
+(defn- train-token-model [model model-name genre n & {:keys [full-model]}]
+  (->> genre
+       (#(db/tokens-by-genre % :field "orth"))
+       ((comp dorun map) #(train! model %)))
+  (compile-model! model-name model)
+  #_(let [model-name (str (string/join "." genre) "-token-" n "-gram")
+          model (if full-model
+                  (build-token-model! :background-model full-model))]))
+
+(defn compile-all-models! [type & {:keys [n] :or {n 3}}]
+  (let [genres (expand-seq-distinct (map natsume-server.models.sql-helpers/ltree->seq (db/get-genres)))
+        #_full-model #_(case type
+                     :char (build-char-model! :n n)
+                     :token (build-token-model! :n n))] ; Add all data to be used as background smoothing model for token n-grams.
     (doseq [genre genres]
-      (let [model-name (string/join "." genre)
-            model (build-char-model!)]
-        (->> genre
-             db/sentences-by-genre
-             ((comp dorun map) #(train! model %)))
-        (compile-model! model-name model)))))
+      (let [model-name (str (string/join "." genre) "-" (name type) "-" n "-gram")
+            model (case type
+                    :char (build-char-model! :n n)
+                    :token (build-token-model! :n n))]
+        (case type
+          :char (train-char-model model model-name genre n #_:full-model #_full-model)
+          :token (train-token-model model model-name genre n #_:full-model #_full-model))))))
+
+(comment
+  (do
+    (compile-all-models! :token)
+    (save-similarity-table! (get-genre-lm-similarities :type :token) :type :token)))
 
 ;; ## Genre similarities API
 
-(defn get-genre-similarities [genre]
+; Binary operators
+(defn dot
+  "Returns the value of dot product of the vectors v1 and v2"
+  [v1 v2]
+  (reduce + (map * v1 v2)))
+
+; Scalar operators
+(defn scale
+  "Returns the vector that is m times the vector v"
+  [m v]
+  (map #(* m %) v))
+
+; Unary operators
+(defn norm
+  "Returns the (Euclidean) length of the vector v"
+  [v]
+  (Math/sqrt (dot v v)))
+
+(defn normalise
+  "Returns a vector of unit length in the same direction as v"
+  [v]
+  (scale (/ 1 (norm v)) v))
+
+(defn normalise-polar
+  "Returns a vector of unit length in the same direction as v"
+  [v]
+  (map #(* (- 0.5 %) 2) (normalise v)))
+
+(defn make-score
+  [v]
+  (let [max-v (apply max v)
+        min-v (apply min v)
+        span (- max-v min-v)
+        factor (/ span 2)]
+    (map #(* 2 (- 0.5 (/ (- % min-v) span))) v)))
+
+(defn get-genre-similarities-all [genre]
   (let [values (get genre-lm-similarities genre)
-        root-value (some #(if (= genre (first %)) (second %)) values)
+        values (let [k (keys values) v (vals values)] (zipmap k (make-score v)))
         similarities (remove #(= genre (first %)) values)]
-    (db/seq-to-tree
+    (tree/seq-to-tree
      (reduce (fn [init [k v]]
                (conj init {:genre k :count v}))
              []
              similarities)
+     :root-values {:count 0}
+     :merge-keys [:count]
+     :merge-fn #(if %1 %1 %2))))
+(def stjc-similarities (delay (get-genre-similarities-all ["科学技術論文"])))
+(defn similarity-score [pos tree]
+  (let [computed-tree (tree/normalize-tree @stjc-similarities tree :boost-factor 1 :update-field :count :update-fn (fnil * 0.0))]
+    computed-tree))
+
+(comment
+  (similarity-score :a (db/get-one-search-token {:orth-base "て" :lemma "て"} :compact-numbers false))
+  )
+
+
+(defn get-genre-similarities [genre]
+  (let [values (get genre-lm-similarities genre)
+        values (let [k (keys values) v (vals values)] (zipmap k (make-score v)))
+        root-value #_1.0 (some #(if (= genre (first %)) (second %)) values)
+        similarities (remove #(= genre (first %)) values)]
+    (tree/seq-to-tree
+     (reduce (fn [init [k v]]
+               (conj init {:genre k :similarity v}))
+             []
+             similarities)
      :root-name genre
-     :root-value root-value
+     :root-values {:similarity root-value}
+     :merge-keys [:similarity]
      :merge-fn #(if %1 %1 %2))))
 
 ;; FIXME now how to map these scores on a (-1,1) scale????? in fact, all other genres are negative, just some more than others. The question is, again, to what does -1 correspond to. With perplexity, we are looking at the average branching factor.
