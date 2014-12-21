6abca81239a4e12467f18d72091e6f23bf1e92db
Bor Hodošček <bor.hodoscek@gmail.com>
2014-12-21 18:28:42 +0900

    refactor out error code and bring evaluation to working state

---
+++ src/natsume_server/nlp/error.clj
@@ -0,0 +1,184 @@
+(ns natsume-server.nlp.error
+  (:require [schema.core :as s]
+            [plumbing.core :refer [?> ?>> map-vals]]
+            [natsume-server.nlp.text :as text]
+            [natsume-server.nlp.annotation-middleware :as anno]
+            [natsume-server.component.database :as db]
+            [natsume-server.nlp.collocations :as coll]
+            [natsume-server.nlp.stats :as stats]
+            [natsume-server.utils.numbers :refer [compact-number]]
+            [clojure.core.reducers :as r]
+            [clojure.set :as set]))
+
+(defn sigma-score [pos n tree]
+  (if-let [freqs (->> tree :children (map (juxt :name :count)) (into {}) seq)]
+    (let [freqs (let [diff (- (count @db/!genre-names) (count freqs))]
+                  (if (pos? diff)
+                    (concat freqs (seq (zipmap (set/difference @db/!genre-names (set (map first freqs))) (repeat diff 0.0))))
+                    freqs))
+          mean (stats/mean (vals freqs))
+          ;;sd (stats/sd (vals freqs))
+          ;; df = 11 (BCCWJ + STJC + Wikipedia?)
+          ;; 0.10      	0.05 	0.025 	0.01 	0.005
+          ;; 17.275 	19.675 	21.920 	24.725 	26.757
+          chisq-line (case pos
+                       :noun 26.757
+                       :verb 26.757
+                       :particle 26.757
+                       :auxiliary-verb 26.757
+                       19.675)
+          chisq-fn #(let [chi (/ (Math/pow (- % mean) 2.0) mean)]
+                     (if (> chi chisq-line)
+                       (- % mean)
+                       nil))
+          chisq-filtered (into {} (r/remove #(nil? (second %)) (map-vals chisq-fn freqs)))
+          good-sum (if-let [good-vals (vals (select-keys chisq-filtered ["白書" "科学技術論文" "法律"]))]
+                     (/ (reduce + good-vals) (count good-vals)))
+          bad-sum (if-let [bad-vals (vals (select-keys chisq-filtered ["Yahoo_知恵袋" "Yahoo_ブログ" "国会会議録"]))]
+                    (/ (reduce + bad-vals) (count bad-vals)))]
+      (-> {:register-score {:good (compact-number (if (number? good-sum) good-sum 0.0))
+                            :bad  (compact-number (if (number? bad-sum) bad-sum 0.0))
+                            :mean (compact-number (if (number? mean) mean 0.0))
+                            :verdict (cond
+                                       (and (and good-sum (>= good-sum 0.0)) (and bad-sum (neg? bad-sum))) true
+                                       (and (and good-sum (<= good-sum 0.0)) (and bad-sum (pos? bad-sum))) false
+                                       :else nil)}
+           :found? true}
+          (?> (> n 1) (assoc :stats (map-vals compact-number (select-keys tree [:count :mi :t :llr]))))))
+    {:found? false}))
+
+(defn mi-score [conn collocation]
+  ;; If no gram found in 1-gram DB, use 1 or 0.5 for freq.
+  (let [n (count (:type-vector collocation))
+        strings [:string-1 :string-2 :string-3 :string-4]
+        ;; Get counts for each string-*, if possible.
+        db-results (map (partial db/query-collocations-tree conn)
+                        (for [i (take n (range (count strings)))]
+                          (-> (apply dissoc collocation strings)
+                              (assoc :measure #{:count :mi}
+                                     :normalize? false
+                                     :type (nth (:type-vector collocation) i)
+                                     :string-1 ((nth strings i) collocation)))))
+        freqs (->> db-results (map :count) (map #(if (or (nil? %) (zero? %)) 1.0 %)))
+        f-xx (get @db/!tokens-by-gram n) #_(:count ((:type collocation) @!gram-totals)) ;; Count for all genres.
+        mi-scores (let [f-ii 1.0
+                        f-ix (first freqs)
+                        f-xi (if (= 2 (count freqs)) (nth freqs 1) (nth freqs 2))]
+                    (select-keys
+                      (stats/compute-association-measures
+                        (stats/expand-contingency-table
+                          {:f-ii f-ii :f-ix f-ix :f-xi f-xi :f-xx f-xx}))
+                      [:mi :t :llr]))]
+    {:stats (map-vals compact-number mi-scores)
+     :found? false}))
+
+(defn collocation-register-score [conn query]
+  (let [collocation (-> (zipmap [:string-1 :string-2 :string-3 :string-4]
+                                (mapcat (fn [part] (remove nil? [(:head-string part) (:tail-string part)]))
+                                        (:data query)))
+                        (assoc :compact-numbers false
+                               :measure #{:count :mi :f-xi :f-ix}
+                               :type-vector (:type query)
+                               :type (->> (:type query)
+                                          (map name)
+                                          (clojure.string/join "-")
+                                          keyword)))
+        tree (db/query-collocations-tree conn collocation)
+        n (count (:type-vector collocation))]
+    (if (seq (:children tree))
+      (sigma-score :collocation n tree)
+      (if (> n 1)
+        (mi-score conn collocation)
+        -2))))
+
+(s/defn token-register-score
+  "Old formula, but includes measures other than chi-sq."
+  [conn query]
+  (let [results (db/get-one-search-token conn query :compact-numbers false)]
+    (sigma-score (:pos query) 1 results)))
+
+(s/defn score-sentence [conn tree sentence]
+  (let [tokens (->> tree
+                    (mapcat :tokens)
+                    (remove (fn [{:keys [pos pos-1 pos-2]}] (or (= pos :symbol) (and (= pos-1 "助詞") (or (= pos-2 "格助詞") (= pos-2 "係助詞"))))))
+                    (pmap (fn [m]
+                            (let [register-score (token-register-score conn m)
+                                  response {:type :token
+                                            :tags (:tags m)
+                                            :pos (:pos m)
+                                            :begin (:begin m)
+                                            :end (:end m)
+                                            :lemma (:lemma m)
+                                            :string (:orth m)}]
+                              (if (map? register-score)
+                                (merge response register-score)
+                                response))))
+                    (into []))
+        collocations (->> tree
+                          coll/extract-collocations
+                          (filter identity) ;; FIXME ぞれ NPE problem
+                          (remove (fn [m] (= (:type m) [:verb :verb]))) ;; FIXME
+                          (pmap (fn [m]
+                                  (let [record
+                                        {:type :collocation
+                                         :pos (:type m)
+                                         :tags (:tags m)
+                                         :parts (->> m
+                                                     :data
+                                                     (r/map (fn [part]
+                                                              (let [begin (or (:head-begin part) (:tail-begin part))
+                                                                    end   (or (:head-end part)   (:tail-end part))
+                                                                    pos   (or (:head-pos part)   (:tail-pos part))
+                                                                    tags  (or (:head-tags part)  (:tail-tags part))
+                                                                    lemma (or (:head-string part) (:tail-string part))]
+                                                                {:begin begin
+                                                                 :end end
+                                                                 :pos pos
+                                                                 :tags tags
+                                                                 :lemma lemma
+                                                                 :string (subs sentence begin end) #_(:head-string part) #_(:tail-string part)})))
+                                                     (into []))}
+                                        register-score (collocation-register-score conn m)]
+                                    (-> record
+                                        (assoc :string (clojure.string/join (map :string (:parts record))))
+                                        (?> (map? register-score) (merge register-score))))))
+                          (into []))]
+    (concat tokens collocations)))
+
+(s/defn get-error
+        [conn :- s/Any
+         text :- s/Str]
+  (if-let [paragraphs (->> text vector text/lines->paragraph-sentences)]
+    (let [update-positions (fn [m offset] (-> m (update-in [:begin] + offset) (update-in [:end] + offset)))
+
+          [scored-sentences parsed-tokens]
+          (loop [ss (for [paragraph paragraphs sentence paragraph] sentence)
+                 offset 0
+                 parsed-tokens []
+                 results []]
+            (if-let [s (first ss)]
+              (let [tree (anno/sentence->tree s)
+                    token-seq (mapv #(select-keys % [:orth :orth-base :lemma :pos-1 :pos-2 :c-form :c-type :tags]) (mapcat :tokens tree))
+                    scored-s (score-sentence conn tree s)
+                    length-s (count s)
+                    new-offset (+ offset length-s)
+                    nl? (and (< new-offset (count text)) (= \newline (first (subs text new-offset (inc new-offset)))))]
+                (recur (next ss)
+                       (+ new-offset (if nl? 1 0))
+                       (concat parsed-tokens (if nl? (conj token-seq {:orth "\n" :orth-base "\n" :lemma "\n" :pos-1 "補助記号" :pos-2 "*" :c-form "*" :c-type "*"}) token-seq))
+                       (concat results
+                               (map (fn [m] (case (:type m)
+                                              :token (update-positions m offset)
+                                              :collocation (update-in m [:parts]
+                                                                      (fn [parts]
+                                                                        (mapv (fn [part]
+                                                                                (update-positions part offset))
+                                                                              parts)))))
+                                    scored-s))))
+              [results (vec parsed-tokens)]))
+
+          bad-morphemes (->> scored-sentences
+                             (r/filter #(or (:register-score %) (:stats %) #_(and (:stats %) #_(> (-> % :stats :mi) 5.0))))
+                             (into []))]
+      {:results bad-morphemes :parsed-tokens parsed-tokens})
+    (throw (Exception. (pr-str text)))))
--- src/natsume_server/nlp/evaluation.clj
+++ src/natsume_server/nlp/evaluation.clj
@@ -2,23 +2,106 @@
   (:require [schema.core :as s]
             [clojure.java.io :as io]
             [clojure.data.csv :as csv]
+            [clojure.core.match :refer [match]]
+            [clojure.core.reducers :as r]
 
-            [natsume-server.endpoint.api :as api]))
+            [natsume-server.component.database :as db]
+            [natsume-server.nlp.error :as error]))
 
-;; TODO go over adverb list and use error API to evaluate algo
-(s/defschema Adverb {:adverb s/Str :category s/Str})
-(s/defschema ScoredAdverb (assoc Adverb :score (s/enum :NA :good :bad)))
-(s/defn get-adverbs :- [Adverb]
+(s/defschema Token
+             {:orth-base s/Str :lemma s/Str :pos-1 s/Str
+              :academic-written (s/maybe s/Bool) :normal-written (s/maybe s/Bool)
+              :public-spoken (s/maybe s/Bool) :normal-spoken (s/maybe s/Bool)})
+
+(s/defschema ScoredToken
+             (assoc Token :academic-score (s/maybe s/Bool) :colloquial-score (s/maybe s/Bool)))
+
+(s/defn get-tokens :- [Token]
   [adverb-file :- s/Str]
   (->> (with-open [adverb-reader (io/reader adverb-file)]
          (doall (csv/read-csv adverb-reader :separator \tab :quote 0)))
-       (zipmap [:adverb :category])
+       (r/drop 1)
+       (r/map (fn [[表層形 左文脈ID 右文脈ID コスト 品詞大分類 品詞中分類 品詞小分類 品詞細分類 活用型 活用形 語彙素読み 語彙素 書字形出現形 発音形出現形 書字形基本形 発音形基本形 語種 語頭変化型 語頭変化形 語末変化型 語末変化形 アカデミックな書き言葉 一般的な書き言葉 公的な話し言葉 日常の話し言葉 備考]]
+                ;; We only need a few features to match.
+                {:orth-base 書字形出現形
+                 :lemma 語彙素
+                 :pos-1 品詞大分類
+                 :academic-written (case アカデミックな書き言葉   "○" true "×" false nil)
+                 :normal-written   (case 一般的な書き言葉      "○" true "×" false nil)
+                 :public-spoken    (case 公的な話し言葉       "○" true "×" false nil)
+                 :normal-spoken    (case 日常の話し言葉       "○" true "×" false nil)}))
        (into [])))
 
-(s/defn score-adverbs :- [ScoredAdverb]
-  [conn :- s/Any
-   adverbs :- [Adverb]]
-  (for [{:keys [adverb] :as a} adverbs]
-    (api/get-text-register conn {:body adverb})))           ;; FIXME any way of optimizing the parameters of the scoring function?
+(def conn (db/druid-pool {:subname "//localhost:5432/natsumedev"
+                          :user "natsumedev"
+                          :password "riDJMq98LpyWgB7F"}))
+
+(s/defn score-tokens :- [ScoredToken]
+  [;;conn :- s/Any
+   tokens :- [Token]]
+  (for [{:keys [orth] :as m} tokens]                      ;; There will be duplicates for lemmas, so going with orth (probably not good idea though, should just directly query database and not use MeCab/CaboCha at all....)
+    (let [score (->> m
+                     (error/token-register-score conn)
+                     ;;(error/get-error conn)
+                     ;;:results
+                     ;;first
+                     :register-score
+                     :verdict)]
+      ;;(println (error/token-register-score conn m))
+      (assoc m :academic-score score :colloquial-score (case score true false false true nil nil)))))           ;; FIXME any way of optimizing the parameters of the scoring function?
+
+(comment
+  (score-tokens (get-tokens "data/unidic-adverb1219.tsv"))
+  (filter #(and (:score %) (:normal-spoken %)) (score-tokens (get-tokens "data/unidic-adverb1219.tsv"))))
+
+(s/defn save-table
+  [fn :- s/Str
+   tokens :- [ScoredToken]]
+  (let [ks (keys ScoredToken)]
+    (with-open [w (io/writer fn)]
+      (csv/write-csv w (into [(mapv name ks)] (mapv #(mapv % ks) tokens)) :separator \tab :quote 1))))
+
+(comment (save-table "data/unidic-adverb1219-scored.tsv"
+                     (score-tokens (get-tokens "data/unidic-adverb1219.tsv"))))
 
 ;; TODO precision/recall
+
+(s/defschema ConfusionMatrix {:tp s/Num :fp s/Num :fn s/Num :tn s/Num :NA s/Num})
+(s/defn confusion-matrix :- ConfusionMatrix
+  [tokens :- [ScoredToken]
+   true-field :- s/Keyword
+   predicted-field :- s/Keyword]
+  "Calculates the confusion matrix from tokens given the true and predicted fields."
+  (r/reduce
+    (fn [cm token]
+      (let [true-val (get token true-field)
+            predicted-val (get token predicted-field)]
+        (update cm
+                (match [true-val predicted-val]
+                       [true   true] :tp [false  true] :fp
+                       [true  false] :fn [false false] :tn
+                       :else :NA)
+                inc)))
+    {:tp 0 :fp 0 :fn 0 :tn 0 :NA 0}
+    tokens))
+
+(s/defn precision :- s/Num
+  [cm :- ConfusionMatrix]
+  (let [{:keys [tp fp]} cm]
+    (double (/ tp (+ tp fp)))))
+
+(s/defn recall :- s/Num
+  [cm :- ConfusionMatrix]
+  (let [{:keys [tp fn]} cm]
+    (double (/ tp (+ tp fn)))))
+
+(s/defn f1 :- s/Num
+  [cm :- ConfusionMatrix]
+  (/ (* 2.0 (precision cm) (recall cm))
+     (+ (precision cm) (recall cm))))
+
+(comment
+  (f1 (confusion-matrix (score-tokens (get-tokens "data/unidic-adverb1219.tsv")) :normal-spoken :colloquial-score))
+  (f1 (confusion-matrix (score-tokens (get-tokens "data/unidic-adverb1219.tsv")) :academic-written :academic-score)))
+
+(s/set-fn-validation! true)
