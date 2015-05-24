f0aebaa5f501e016d7f12fa2cfdaf716a9c6a781
Bor Hodošček <bor.hodoscek@gmail.com>
2015-05-24 16:07:00 +0900

    removing unused ns

--- src/natsume_server/endpoint/webapp.clj
+++
@@ -1,6 +0,0 @@
-(ns natsume-server.endpoint.webapp
-  (:require [compojure.core :refer :all]))
-
-(defn webapp-endpoint [config]
-  (routes
-   (GET "/" [] "Welcome to the client-side component of Natsume")))
