31fe238bc93b99d3e251b1abd54597324c49fb4f
Bor Hodošček <bor.hodoscek@gmail.com>
2015-05-24 16:07:14 +0900

    previous config

--- src/natsume_server/main.clj
+++ src/natsume_server/main.clj
@@ -1,15 +1,12 @@
 (ns natsume-server.main
   (:gen-class)
   (:require [com.stuartsierra.component :as component]
-            [duct.middleware.errors :as errors]
             [environ.core :refer [env]]
             [natsume-server.system :as system]
             [clojure.java.io :as io]))
 
 (def config
-  {:http {:port (some-> env :port Integer.)}
-   :app  {:middleware     [[errors/wrap-hide-errors :internal-error]]
-          :internal-error (io/resource "errors/500.html")}})
+  {:http {:port (some-> env :port Integer.)}})
 
 (defn -main [& args]
   (let [system (system/new-system config)]
