ff6b55937d93c35bd6c28e2c8671ae19431c95b6
Bor Hodošček <bor.hodoscek@gmail.com>
2016-01-06 16:50:55 +0900

    added experimental pretty-print? switch

--- src/natsume_server/config.clj
+++ src/natsume_server/config.clj
@@ -2,7 +2,7 @@
   (:require [environ.core :refer [env]]))
 
 (def defaults
-  ^:displace {:http {:port 3000}
+  ^:displace {:http {:port 3000 :pretty-print? true}
               :db   {:password "riDJMq98LpyWgB7F",
                      :user "natsumedev",
                      :subname "//localhost:5432/natsumedev"},})
