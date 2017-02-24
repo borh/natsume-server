7816e8e65f72c28a12c41c7b9e5a5d33618d7391
Bor Hodošček <bor.hodoscek@gmail.com>
2017-02-24 16:24:18 +0900

    added working word2vec version

--- src/natsume_server/nlp/word2vec.clj
+++ src/natsume_server/nlp/word2vec.clj
@@ -1,36 +1,38 @@
 (ns natsume-server.nlp.word2vec
   (:require [schema.core :as s]
             [plumbing.core :refer [for-map]]
+            [clojure.string :as str]
             [clojure.java.io :as io]
             [byte-streams :refer [convert]]
-            [natsume-server.nlp.importers.local :as local]
-            [clojure.string :as str]
             [me.raynes.fs :as fs]
+            [clojure.data.csv :as csv]
             [mount.core :refer [defstate]]
-            [natsume-server.config :refer [config]])
-  (:import [org.deeplearning4j.models.word2vec.wordstore.inmemory InMemoryLookupCache]
+            [natsume-server.config :refer [config]]
+            [natsume-server.nlp.importers.local :as local])
+  (:import [org.nd4j.linalg.factory Nd4j]
+           [org.nd4j.linalg.api.ndarray INDArray]
+           [org.nd4j.linalg.indexing NDArrayIndex]
+           [org.nd4j.linalg.dimensionalityreduction PCA]
+           [org.deeplearning4j.plot BarnesHutTsne]
+           [org.deeplearning4j.models.word2vec.wordstore.inmemory InMemoryLookupCache]
            [org.deeplearning4j.text.sentenceiterator LineSentenceIterator BasicLineIterator]
            [org.deeplearning4j.text.tokenization.tokenizerfactory TokenizerFactory DefaultTokenizerFactory]
            [org.deeplearning4j.models.word2vec.Word2Vec$Builder]
            [org.deeplearning4j.models.word2vec Word2Vec]
            [org.deeplearning4j.models.embeddings.loader WordVectorSerializer]))
 
-(hugsql/def-db-fns "natsume_server/nlp/sql/utils.sql")
-
-(defn iterate-lines []
-  (convert (for [token-sentence (token-stream connection)] (str/join " " (:string_agg token-sentence))) java.io.InputStream))
-
 ;; '/tmp/natsume_temp_corpus.txt'
 
-(defn export-corpus!
-  [unit-type features]
-  (export-corpus connection
-                 {:table (name unit-type) ; :unigrams/:tokens
-                  :features (first (map name features)) ; FIXME made real vector (:orth)
-                  :export-path (format "%s/corpus-%s-%s.txt"
-                                       (System/getProperty "user.dir")
-                                       (name unit-type)
-                                       (str/join "_" (map name features)))}))
+(comment
+  (defn export-corpus!
+    [unit-type features]
+    (export-corpus connection
+                   {:table (name unit-type) ; :unigrams/:tokens
+                    :features (first (map name features)) ; FIXME made real vector (:orth)
+                    :export-path (format "%s/corpus-%s-%s.txt"
+                                         (System/getProperty "user.dir")
+                                         (name unit-type)
+                                         (str/join "_" (map name features)))})))
 
 (s/defn train :- Word2Vec
   [corpus-path :- s/Str]
@@ -50,7 +52,7 @@
          (.windowSize 10)
          (.layerSize 100)
          (.iterations 1)
-         (.iterate #_(BasicLineIterator. (iterate-lines)) (LineSentenceIterator. (io/file corpus-path)))
+         (.iterate #_(BasicLineIterator. (iterate-lines)) (LineSentenceIterator. (io/file corpus-path))) ;; FIXME Seq iterator for local/tokens-stream
          (.tokenizerFactory (DefaultTokenizerFactory.))
          (.build))]
     (println "Setup finished, fitting...")
@@ -83,32 +85,133 @@
         (save-model! model model-path)
         model))))
 
-(defstate !word2vec-models :start
-  (for-map [{:keys [unit-type features] :as m} (:word2vec config)]
-      m (load-or-train! unit-type features)))
+(defstate ^{:on-reload :noop}
+  !word2vec-models :start
+  (when (:server config)
+    (for-map [{:keys [unit-type features] :as m} (:word2vec config)]
+        m (load-or-train! unit-type features))))
 
 ;; Wrapper for functions defined in http://deeplearning4j.org/doc/org/deeplearning4j/models/embeddings/wordvectors/WordVectorsImpl.html
 
 (defn similarity
   [unit-type features token-a token-b]
-  (.similarity ^Word2Vec ({:unit-type unit-type :features features}
-                          @!word2vec-models)
+  (.similarity ^Word2Vec
+               (get !word2vec-models
+                    {:unit-type unit-type :features features})
                token-a
                token-b))
 
-(defn nearest-tokens ;; TODO .wordsNearestSum
+(defn token-nearest-tokens ;; TODO .wordsNearestSum
   [unit-type features token n]
-  (.wordsNearest ^Word2Vec ({:unit-type unit-type :features features}
-                            @!word2vec-models)
+  (.wordsNearest ^Word2Vec
+                 (get !word2vec-models
+                      {:unit-type unit-type :features features})
                  ^String token
                  (int n)))
 
-(defn similar-tokens-with-accuracy
+(comment
+  (defn tokens-nearest-tokens
+    [unit-type features tokens n]
+    (.wordsNearest ^Word2Vec
+                   (get !word2vec-models
+                        {:unit-type unit-type :features features})
+                   (java.util.ArrayList. tokens) ;;
+                   (int n))))
+
+(defn similar-tokens-with-accuracy ;; FIXME
   [unit-type features token accuracy]
-  (.similarWordsInVocabTo ^Word2Vec ({:unit-type unit-type :features features}
-                                     @!word2vec-models)
+  (println [unit-type features token accuracy])
+  (.similarWordsInVocabTo ^Word2Vec
+                          (get !word2vec-models
+                               {:unit-type unit-type :features features})
                           token
                           accuracy))
+
+(defn tokens-nearest-pos-neg-tokens
+  [unit-type features positive-tokens negative-tokens n]
+  (.wordsNearest ^Word2Vec
+                 (get !word2vec-models
+                      {:unit-type unit-type :features features})
+                 positive-tokens ;; TEST: (java.util.ArrayList.)
+                 negative-tokens
+                 (int n)))
+
+(defn get-token-vector
+  [unit-type features token]
+  (let [model
+        ^Word2Vec(get !word2vec-models {:unit-type unit-type :features features})]
+    (.getWordVectorMatrix model token)))
+
+(defn pca
+  [unit-type features n token]
+  (let [nearest-tokens (token-nearest-tokens unit-type features token n)
+        matrix ^INDArray (Nd4j/vstack
+                          (for [token nearest-tokens]
+                            (get-token-vector unit-type features token)))]
+    (PCA/pca matrix 5 true) ;; PCA in Nd4j is currently broken
+    ))
+
+(defn ^BarnesHutTsne tsne-builder []
+  (-> (org.deeplearning4j.plot.BarnesHutTsne$Builder.)
+      (.setMaxIter 500)
+      (.stopLyingIteration 250)
+      (.learningRate 500)
+      (.useAdaGrad false)
+      (.theta 0.5)
+      (.setMomentum 0.5)
+      (.normalize true)
+      #_(.usePca true) ;; deprecated?
+      (.build)))
+
+(defn tsne
+  #_([unit-type features] (tsne unit-type features 1000))
+  #_([unit-type features n]
+     (let [tsne (tsne-builder)
+           model ^Word2Vec (get !word2vec-models {:unit-type unit-type :features features})]
+
+       (-> model
+           (.lookupTable)
+           (.plotVocab tsne n (io/file "tsne-plot.csv")))
+
+       (mapv (fn [[x y word]]
+               {:x x :y y :word word})
+             (with-open [r (io/reader "tsne-plot.csv")]
+               (doall (csv/read-csv r :separator \, :quote 0))))))
+  ([unit-type features token]
+   (tsne unit-type features token 50 2))
+  ([unit-type features token n-neighbours]
+   (tsne unit-type features token n-neighbours 2))
+  ([unit-type features token n-neighbours n-dims]
+   (let [nearest-tokens (token-nearest-tokens unit-type features token n-neighbours)
+         matrix ^INDArray (Nd4j/vstack
+                           (for [token nearest-tokens]
+                             (get-token-vector unit-type features token)))
+         bht (tsne-builder)]
+
+     (.fit bht ^INDArray matrix n-dims)
+
+     (let [coords (.getData bht)
+           rows (.rows coords)]
+       (for [i (range rows)]
+         (merge {:token (nth nearest-tokens i)}
+                (zipmap [:x :y :z] (map #(.getDouble coords i %) (range n-dims)))))))))
+
+;; INDArray matrix, int nDims
+;; BarnesHutTsne.fit(matrix, nDims) -> Void
+;; .Y -> output data
+
+
+
+;; Nd4j.vstack
+
+
+;; WeightLookupTable weightLookupTable = wordVectors.lookupTable();
+;; Iterator<INDArray> vectors = weightLookupTable.vectors();
+;; INDArray wordVector = wordVectors.getWordVectorMatrix("myword");
+;; double[] wordVector = wordVectors.getWordVector("myword");
+
+
+;; TODO https://github.com/deeplearning4j/dl4j-examples/blob/master/dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/paragraphvectors/ParagraphVectorsClassifierExample.java
 
 (comment
   (def t (train))
