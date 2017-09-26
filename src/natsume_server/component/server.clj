64ee6a247ead6eef3c1374438feb22b9ff865b39
Bor Hodošček <bor.hodoscek@gmail.com>
2017-09-26 13:15:15 +0900

    added better server defaults for https

--- src/natsume_server/component/server.clj
+++ src/natsume_server/component/server.clj
@@ -3,9 +3,10 @@
             [aleph.http :as http]
             [natsume-server.endpoint.api :refer [api-routes]]
             [natsume-server.component.sente :as comm]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults secure-site-defaults site-defaults]]
             [ring.middleware.json :refer [wrap-json-params]]
             [ring.middleware.cors :refer [wrap-cors]]
+            [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
             [ring.util.response :as response]
             [ring.logger.timbre :as logger]
             [buddy.auth :refer [authenticated? throw-unauthorized]]
@@ -42,11 +43,7 @@
       ((:ring-ajax-post-fn comm/channel) ring-req)))
   (POST "/api/authenticate" ring-req
     (create-authentication-token ring-req))
-  (OPTIONS "/api/authenticate" ring-req {:status 200
-                                         :headers {"Access-Control-Allow-Origin" "*"
-                                                   "Access-Control-Allow-Headers" "Content-Type"
-                                                   "Access-Control-Allow-Methods" "GET,POST,OPTIONS"}
-                                         :body "preflight complete"})
+  (OPTIONS "/api/authenticate" ring-req {:status 200 :body "preflight complete"})
   (GET "/api/auth-files/:filename" [filename :as ring-req]
     (if-not (auth/authfn ring-req (-> ring-req :params :client-id))
       (throw-unauthorized)
@@ -65,9 +62,14 @@
                 :access-control-allow-headers ["Content-Type"])
                (wrap-authentication auth/backend)
                (wrap-authorization auth/backend)
-               (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
-               (handler/site)
+               (wrap-defaults (-> (if (= :prod (:profile config))
+                                    (assoc secure-site-defaults :proxy true)
+                                    site-defaults)
+                                  (assoc-in [:security :anti-forgery] false)
+                                  (assoc :session false)))
+               #_(wrap-anti-forgery {:read-token (fn [request] (get-in request [:headers "x-forgery-token"]))})
                (wrap-json-params)
+               (handler/site)
                (http/start-server {:port (-> config :http :port)})))
   :stop (when (:server config)
           (.close server)))
