bb47aca2eeb3082fa26dcc1c9f81e52520ebb29e
Bor Hodošček <dev@bor.space>
2019-11-06 14:43:21 +0900

    update updater

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 clojure -Aoutdated -a outdated -a web,webdev,test,uberjar,depstar,runner --resolve-virtual
+clojure -Aoutdated -a outdated -a web,webdev,test,uberjar,depstar,runner
